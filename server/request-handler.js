2556ba99322dc88638f798d794cfdd5b663c77f5
Hack Reactor Student <student@hackreactor.com>
2017-10-31 16:06:41 -0700

    Return out of POST callback and therefore send proper status code

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,11 +11,8 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
-var messageData = require('./messages.js');
+// var messageData = require('./messages.js');
 
-var saveMessage = function(string) {
-  messageData.messages.push(string);
-};
 
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -34,35 +31,16 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
+var messages = [];
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
+
+var requestHandler = function(request, response) {
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
-  // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
   headers['Content-Type'] = 'application/json';
 
-  // The outgoing status.
   var statusCode;
-  // console.log(request);
   
   if (request.method === 'OPTIONS') {
     statusCode = 200;
@@ -74,46 +52,31 @@ var requestHandler = function(request, response) {
     if (request.url === '/classes/messages') {
       statusCode = 200;
       response.writeHead(statusCode, headers);
-      // console.log(messageData.responseData);
-      response.end(JSON.stringify(messageData.responseData));
+      response.end(JSON.stringify({results: messages}));
       return;
     } 
   } else if (request.method === 'POST') {
     if (request.url === '/classes/messages' ) {
+      var saveMessage = function(message) {
+        messages.push(message);
+        statusCode = 201;
+        response.writeHead(statusCode, headers);
+        response.end();
+      };
       
-      request.on('data', saveMessage);
-    
-      statusCode = 201;
-      
-      response.writeHead(statusCode, headers);
-      response.end();
+      var body = '';
+      request.on('data', function(chunk) {
+        body += chunk;
+      });
+      request.on('end', function() {
+        saveMessage(JSON.parse(body));
+      });
       return;
-      
-    // } else if (request.uri === 'http://127.0.0.1:3000/classes/messages') {
-    //   var messageObj = {
-    //     username: request.json.username, 
-    //     message: request.json.message 
-    //   }; 
-      
-    //   messageData.messages.push(messageObj); 
-    //   statusCode = 201;
-    //   response.writeHead(statusCode, headers);
-    //   response.end();
-    //   return;
     }
   } 
   statusCode = 404;
   response.writeHead(statusCode, headers);
   response.end();
-  
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  // response.end();
 };
 
 
